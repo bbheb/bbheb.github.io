bdeb3440d6446593bacf11ffa7af9ff3b925d4bb
Martin Zhang <mmtscn@gmail.com>
2021-02-10 08:14:02 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-$(function(){
-  $('img').click(function(){
-    $(this).toggleClass('min');
-    $(this).toggleClass('max');
-  });
-});
+// 图片点击放大
+var $ = function( obj ){
+                return document.querySelector( obj );
+            }
+
+            var initWindowWithIMGAndShow = function( src ){
+                $('.window img').src = src;
+                $('.window').classList.add('window-show');
+            }
+
+            $('img').addEventListener('click', function(){
+                var src = this.src;
+                initWindowWithIMGAndShow( src );
+            }, false)
+
+            $('.cloes-tag').addEventListener('click', function(){
+                $('.window').classList.remove('window-show');
+            }, false)
