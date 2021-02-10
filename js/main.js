6c7acf3be72bbfcc3810a34d188316d82e1462f1
Martin Zhang <mmtscn@gmail.com>
2021-02-10 08:15:21 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // 图片点击放大
 var $ = function( obj ){
-                return document.querySelector( obj );
-            }
+  return document.querySelector( obj );
+}
 
-            var initWindowWithIMGAndShow = function( src ){
-                $('.window img').src = src;
-                $('.window').classList.add('window-show');
-            }
+var initWindowWithIMGAndShow = function( src ){
+  $('.window img').src = src;
+  $('.window').classList.add('window-show');
+}
 
-            $('img').addEventListener('click', function(){
-                var src = this.src;
-                initWindowWithIMGAndShow( src );
-            }, false)
+$('img').addEventListener('click', function(){
+  var src = this.src;
+  initWindowWithIMGAndShow( src );
+}, false)
 
-            $('.cloes-tag').addEventListener('click', function(){
-                $('.window').classList.remove('window-show');
-            }, false)
+$('.cloes-tag').addEventListener('click', function(){
+  $('.window').classList.remove('window-show');
+}, false)
