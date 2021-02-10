fa036a47819e9907119d9173eddd1e148e95c93e
Martin Zhang <mmtscn@gmail.com>
2021-02-10 08:16:27 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // 图片点击放大
-var $ = function( obj ){
-  return document.querySelector( obj );
+var $ = function(obj) {
+  return document.querySelector(obj);
 }
 
-var initWindowWithIMGAndShow = function( src ){
+var initWindowWithIMGAndShow = function(src) {
   $('.window img').src = src;
   $('.window').classList.add('window-show');
 }
 
-$('img').addEventListener('click', function(){
+$('img').addEventListener('click', function() {
   var src = this.src;
-  initWindowWithIMGAndShow( src );
+  initWindowWithIMGAndShow(src);
 }, false)
 
-$('.cloes-tag').addEventListener('click', function(){
+$('.cloes-tag').addEventListener('click', function() {
   $('.window').classList.remove('window-show');
 }, false)
