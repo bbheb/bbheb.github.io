409a58cb9fe95f77da4ec6d150664cecb228be85
Martin Zhang <mmtscn@gmail.com>
2021-03-02 09:38:39 -0500

    update

--- js/checkAns.js
+++ js/checkAns.js
@@ -52,7 +52,7 @@ $(() => {
   document.getElementById('para-nav').innerHTML = paraNavHTML;
 
 
-  // Mouse over ❓ or ❌ to reveal the answer key. Buttons to reveal/clear all answer keys.
+  // Click ❓ or ❌ to reveal the answer key. Buttons to reveal/clear all answer keys.
   let revealAnsTipHTML = '<b>* Click ❓ or ❌ to reveal individual answer key.</b></br>\
   <button type="button" class="btn btn-success" onclick="revealAllAnswers()">Click Me</button> to Reveal All Answers;\
   <button type="button" class="btn btn-info" onclick="location.reload()">Click Me</button> to Clear All Answers.\
