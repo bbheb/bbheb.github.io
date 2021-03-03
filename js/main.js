326ac6bae6492281c235675480d933a76f3d0472
Martin Zhang <mmtscn@gmail.com>
2021-03-02 21:53:46 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(() => {
   // navigator on paradigm practice pages
   let paraNavHTML = '<p style="font-size:14px; text-align:right" ><a href="../../index.html">Return to the Main Resource Page</a></p>'
-  document.getElementById('para-nav').innerHTML = paraNavHTML;
+  $("#para-nav").html(paraNavHTML);
+
 });
+
+function loadSourceHTML(sourceURL,sourceID,currentPageID){
+  $.get(sourceURL, function(html){
+    let $section = $(html).filter("#" + sourceID).html();
+    if(typeof $section !== 'undefined'){
+      $("#" + currentPageID).html($section);
+    } else {
+      $.get(sourceURL, function(html){
+        let $section = $(html).find("#" + sourceID).html();
+        $("#" + currentPageID).html($section);
+      });
+    }
+  });
+}
