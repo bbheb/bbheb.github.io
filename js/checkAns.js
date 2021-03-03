65fa4786cd5ec08c2b695033d9db42f4e2b42ac0
Martin Zhang <mmtscn@gmail.com>
2021-03-03 17:43:14 -0500

    update

--- js/checkAns.js
+++ js/checkAns.js
@@ -59,7 +59,7 @@ $(() => {
   // Click ❓ or ❌ to reveal the answer key. Buttons to reveal/clear all answer keys.
   let revealAnsTipHTML = '<b>* Click ❓ or ❌ to reveal individual answer key.</b></br>\
   <button type="button" class="btn btn-success" onclick="revealAllAnswers()">Click Me</button> to Reveal All Answers;\
-  <button type="button" class="btn btn-info" onclick="location.reload()">Click Me</button> to Clear All Answers.\
+  <button type="button" class="btn btn-info" onclick="document.location.reload()">Click Me</button> to Clear All Answers.\
   </br></br>'
   $('#revealAnsTip').html(revealAnsTipHTML);
 
