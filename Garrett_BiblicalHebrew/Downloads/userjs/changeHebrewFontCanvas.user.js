59ce60f5c419e93cbf58c9b4e5f1476e4c82fb19
Martin Zhang <mmtscn@gmail.com>
2021-03-26 11:37:28 -0400

    Create changeHebrewFontCanvas.user.js

---
+++ Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvas.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @author		Martin Zhang
+// @version		0.1
+// @name		changeCanvasMatchQuestionFont
+// @namespace	https://martinmts.github.io/BiblicalHebrew
+// @description	Change the font for matching questions on Canvas, designed for Hebrew vocabulary questions.
+// @match		https://*.instructure.com/courses/*/quizzes/*/take*
+// ==/UserScript==
+
+(function(){
+	'user strict';
+	var x = document.querySelectorAll('.pull-left label');
+	var i;
+	for (i = 0; i < x.length; i++) {
+	  x[i].style.fontFamily = "Times New Roman";
+	  x[i].style.fontSize = "30px";
+	};
+
+})();
