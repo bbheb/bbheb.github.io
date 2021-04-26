32d850f20a90748212f99989f595496f59f8fa7b
bbheb <martin.doulos@gmail.com>
2021-04-26 12:34:53 -0400

    change input field font

--- Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvas.user.js
+++ Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvas.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @author		Martin Zhang
-// @version		0.1
+// @version		0.2
 // @name		changeCanvasMatchQuestionFont
 // @namespace	https://bbheb.github.io
 // @description	Change the font for matching questions on Canvas, designed for Hebrew vocabulary questions.
@@ -15,5 +15,13 @@
 	  x[i].style.fontFamily = "Times New Roman";
 	  x[i].style.fontSize = "30px";
 	};
+
+	var y = document.querySelectorAll('input');
+	var j;
+	for (j = 0; j < y.length; j++) {
+	  y[j].style.fontFamily = "Times New Roman";
+	  y[j].style.fontSize = "30px";
+	};
+
 
 })();
