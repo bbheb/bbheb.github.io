28a7c6f5f62a7abe98e4cc7bf46a25aa0739f319
bbheb <martin.doulos@gmail.com>
2021-06-26 15:32:33 -0400

    Update

--- Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvas.user.js
+++ Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvas.user.js
@@ -3,7 +3,7 @@
 // @version		0.2
 // @name		changeCanvasMatchQuestionFont
 // @namespace	https://bbheb.github.io
-// @description	Change the font for matching questions and user input boxes on Canvas, designed for Hebrew vocabulary questions.
+// @description	Change the font for matching questions (for Hebrew vocabulary questions) and user input boxes (for Hebrew paradigm questions) on Canvas.
 // @match		https://*.instructure.com/courses/*/quizzes/*/take*
 // ==/UserScript==
 
--- Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvasGrader.user.js
+++ Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvasGrader.user.js
@@ -3,39 +3,58 @@
 // @version		0.2
 // @name		changeCanvasMatchQuestionFontGrader
 // @namespace	https://bbheb.github.io
-// @description	Change the font for matching questions (for Hebrew vocabulary questions) and user input boxes (for Hebrew paradigm questions) on Canvas, .
+// @description	Change the font for matching questions (for Hebrew vocabulary questions) and user input boxes (for Hebrew paradigm questions) on Canvas.
 // @match		https://*.instructure.com/courses/*/gradebook/speed_grader*
-// @match		https://*.instructure.com/courses/*/quizzes/*/take*
 // ==/UserScript==
 
 (function(){
 	'use strict';
-	var x = document.querySelectorAll('.pull-left label');
-	var i;
-	for (i = 0; i < x.length; i++) {
-	  x[i].style.fontFamily = "Times New Roman";
-	  x[i].style.fontSize = "30px";
-	};
 
-	var y = document.querySelectorAll('input');
-	var j;
-	for (j = 0; j < y.length; j++) {
-	  y[j].style.fontFamily = "Times New Roman";
-	  y[j].style.fontSize = "30px";
-	};
+	try {
+		var x = document.querySelectorAll(".answer_match_left");
+		var i;
+		for (i = 0; i < x.length; i++) {
+		  x[i].style.fontFamily = "Times New Roman";
+		  x[i].style.fontSize = "30px";
+		};
+	} catch {
 
-	var z = document.querySelectorAll('.answer');
-	var k;
-	for (k = 0; k < z.length; k++) {
-	  z[k].style.fontFamily = "Times New Roman";
-	  z[k].style.fontSize = "30px";
-	};
+	}
 
-	var a = document.querySelectorAll('.answer_text');
-	var b;
-	for (b = 0; b < a.length; b++) {
-	  a[b].style.fontFamily = "Times New Roman";
-	  a[b].style.fontSize = "30px";
-	};
+	try {
+		var y = document.querySelectorAll("input");
+		var j;
+		for (j = 0; j < y.length; j++) {
+		  y[j].style.fontFamily = "Times New Roman";
+		  y[j].style.fontSize = "30px";
+		};
+	
+	} catch {
+
+	}
+
+	try {
+		var z = document.querySelectorAll(".answer");
+		var k;
+		for (k = 0; k < z.length; k++) {
+		  z[k].style.fontFamily = "Times New Roman";
+		  z[k].style.fontSize = "30px";
+		};
+	
+	} catch {
+
+	}
+
+	try {
+		var a = document.querySelectorAll(".answer_text");
+		var b;
+		for (b = 0; b < a.length; b++) {
+		  a[b].style.fontFamily = "Times New Roman";
+		  a[b].style.fontSize = "30px";
+		};
+	
+	} catch {
+		
+	}
 	
 })();
