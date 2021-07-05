976a7c5dd5ee8c0ef65906081462f195e0df4a1d
bbheb <martin.doulos@gmail.com>
2021-07-05 14:52:14 -0400

    Update

---
+++ Garrett_BiblicalHebrew/Downloads/userjs/changeCanvasGraderDBWidth.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @author		Martin Zhang
+// @version		0.1
+// @name		changeCanvasMatchQuestionFontGrader
+// @namespace	https://bbheb.github.io
+// @description	Change the font for matching questions (for Hebrew vocabulary questions) and user input boxes (for Hebrew paradigm questions) on Canvas.
+// @match		https://*.instructure.com/courses/*/gradebook/speed_grader*
+// ==/UserScript==
+
+(function(){
+	'use strict';
+
+	try {
+		var div = document.querySelector("#content > div");
+		div.style.width = "800px";
+	};
+	} catch {
+
+	}
+	
+})();
