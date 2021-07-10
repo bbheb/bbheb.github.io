21f49570c265e5b584a0f056383fd7aefaef19b1
bbheb <martin.doulos@gmail.com>
2021-07-10 15:40:10 -0400

    Update

--- Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvas.user.js
+++ Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvas.user.js
@@ -1,27 +1,33 @@
 // ==UserScript==
 // @author		Martin Zhang
-// @version		0.3
+// @version	0.4
 // @name		changeCanvasMatchQuestionFont
-// @namespace	https://bbheb.github.io
+// @namespace	https://bbheb.github.io/Garrett_BiblicalHebrew/Downloads/userjs/changeHebrewFontCanvas.user.js
 // @description	Change the font for matching questions (for Hebrew vocabulary questions) and user input boxes (for Hebrew paradigm questions) on Canvas.
 // @match		https://*.instructure.com/courses/*/quizzes/*/take*
 // ==/UserScript==
 
 (function(){
 	'use strict';
-	var x = document.querySelectorAll('.pull-left label');
-	var i;
-	for (i = 0; i < x.length; i++) {
-	  x[i].style.fontFamily = "Times New Roman";
-	  x[i].style.fontSize = "30px";
-	};
 
-	var y = document.querySelectorAll('input');
-	var j;
-	for (j = 0; j < y.length; j++) {
-	  y[j].style.fontFamily = "Times New Roman";
-	  y[j].style.fontSize = "30px";
-	};
 
+/*
+ * this user script is based on the one made by [James Jones] at https://github.com/jamesjonesmath/canvancement
+ * It changes the font of the Hebrew words in the Vocabulary Match questions.
+ * It also changes the font of the input box for the Hebrew paradigm questions.
+ */ 
+
+
+  const rules = ['input, div.pull-left > label, div.answer_label { font-family: "Times New Roman"; font-size: 30px !important; }'];
+
+
+  if (rules.length) {
+    const style = document.createElement('style');
+    document.head.appendChild(style);
+    const sheet = style.sheet;
+    for (let i = 0; i < rules.length; i++) {
+      sheet.insertRule(rules[i],i);
+    }
+  }
 
 })();
